ee1c9d582a228e5794e4103bc19f672d014cf21c
Studawatta <st.udawatte@gmail.com>
2022-11-08 10:14:52 +0530

    test

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is home page</h1>
+            <h1>Welcome to the home</h1>
         </div>
     );
 }
