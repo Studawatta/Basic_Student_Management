30ea16d3ac3d6705c3122f124aac131c093ef724
Studawatta <st.udawatte@gmail.com>
2022-11-14 23:43:58 +0530

    navBar

--- frontend/src/components/navBar/Nav.jsx
+++ frontend/src/components/navBar/Nav.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import './Nav.css'
+import './Nav.css';
+import { FaListUl } from "react-icons/fa";
 
 const Nav = () => {
     return (
         <div >
             
             <div className='nav'>
+                <div className='Container'>
                 <div className='navItemCont'>
+                    <FaListUl className='listIcon'/>
+                   
                     <Link className='navItem'>Home</Link>
                     <Link className='navItem'>About</Link>
                     <Link className='navItem' id="cont">Contact</Link>
@@ -16,7 +20,14 @@ const Nav = () => {
                     <Link className='signIn'>Sign In</Link>
                     <Link className='reg'>Register</Link>
                 </div>
+                </div>
+               
             </div>
+                  <div className='navDropDown'>
+                    <Link className='navItemD'>Home</Link>
+                    <Link className='navItemD'>About</Link>
+                    <Link className='navItemD' id="cont">Contact</Link>
+                    </div>
                 
             
         </div>
